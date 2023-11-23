d7fd7d6473f51f3d8d92bfabd9ae6a8149500cac
Gautam Anand <gautamgxtv@gmail.com>
2023-11-23 16:47:42 -0600

    Test update.sh

---
+++ update.sh
@@ -0,0 +1,20 @@
+# update.sh
+echo "                           _ _           _   _   _        _
+ _____      _____  _ __ __| | |__   __ _| |_| |_| | ___  (_) ___
+/ __\ \ /\ / / _ \| '__/ _` | '_ \ / _` | __| __| |/ _ \ | |/ _ \
+\__ \\ V  V / (_) | | | (_| | |_) | (_| | |_| |_| |  __/_| | (_) |
+|___/ \_/\_/ \___/|_|  \__,_|_.__/ \__,_|\__|\__|_|\___(_)_|\___/
+A game by Gautam"
+git pull
+# stop all forever processes
+forever stopall
+# start the server
+cd server
+yarn
+yarn foreverprod
+cd ..
+cd api
+yarn
+yarn foreverprod
+cd ..
+echo "Done!"

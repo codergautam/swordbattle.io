200cb42fd0757f35a7978422910663c1c63c9e76
Gautam Anand <gautamgxtv@gmail.com>
2023-11-23 16:50:02 -0600

    Test update.sh

--- update.sh
+++ update.sh
@@ -1,20 +1,38 @@
-# update.sh
-echo "                           _ _           _   _   _        _
+#!/bin/bash
+
+# This script updates the project and restarts the server
+
+# Display a banner
+cat << "EOF"
+                           _ _           _   _   _        _
  _____      _____  _ __ __| | |__   __ _| |_| |_| | ___  (_) ___
 / __\ \ /\ / / _ \| '__/ _` | '_ \ / _` | __| __| |/ _ \ | |/ _ \
 \__ \\ V  V / (_) | | | (_| | |_) | (_| | |_| |_| |  __/_| | (_) |
 |___/ \_/\_/ \___/|_|  \__,_|_.__/ \__,_|\__|\__|_|\___(_)_|\___/
-A game by Gautam"
+
+A game by Gautam
+EOF
+
+# Pull the latest changes from the repository
+echo "Pulling latest changes..."
 git pull
-# stop all forever processes
+
+# Stop all running forever processes
+echo "Stopping all forever processes..."
 forever stopall
-# start the server
+
+# Start the server
+echo "Starting the server..."
 cd server
-yarn
-yarn foreverprod
+yarn install
+yarn run foreverprod
 cd ..
+
+# Start the API
+echo "Starting the API..."
 cd api
-yarn
-yarn foreverprod
+yarn install
+yarn run foreverprod
 cd ..
-echo "Done!"
+
+echo "Update completed successfully!"

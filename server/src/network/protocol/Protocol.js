e54b6682e12cad53af314faeaced065dd8318a62
Gautam Anand <gautamgxtv@gmail.com>
2024-01-10 21:24:42 -0600

    Round sent nums

--- server/src/network/protocol/Protocol.js
+++ server/src/network/protocol/Protocol.js
@@ -3,8 +3,22 @@ const protobuf = require('protobufjs');
 const root = protobuf.loadSync(__dirname + '/schema.proto');
 const ServerMessage = root.lookupType('ServerMessage');
 const ClientMessage = root.lookupType('ClientMessage');
-
+const makeSendable = (data, depth = 0) => {
+  if(depth > 3) return data;
+  const keys = Object.keys(data);
+  for (const key of keys) {
+    if (typeof data[key] === 'number') {
+      data[key] = Math.round(data[key] * 100) / 100;
+    }
+    if(typeof data[key] === 'object') {
+      if(!data[key] || !key) continue;
+      data[key] = makeSendable(data[key], depth + 1);
+    }
+  }
+  return data;
+}
 const encode = (data) => {
+  data = makeSendable(data);
   const message = ServerMessage.create(data);
   return ServerMessage.encode(message).finish();
 }
@@ -18,12 +32,7 @@ const decode = (msg) => {
 let decoded;
   try {
   decoded = ClientMessage.decode(payload);
-  const keys = Object.keys(decoded);
-  for (const key of keys) {
-    if (typeof decoded[key] === 'number') {
-      decoded[key] = parseFloat(decoded[key].toFixed(2));
-    }
-  }
+    decoded = makeSendable(decoded);
   } catch (e) {
     console.log('[Protocol] Decoding error: ', e);
     return null;

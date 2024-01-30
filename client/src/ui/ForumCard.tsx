207e5e173d29bd2ac63e90e06cbe853df2bc5452
Gautam Anand <gautamgxtv@gmail.com>
2024-01-29 21:09:56 -0600

    Minor forum card style impr

--- client/src/ui/ForumCard.tsx
+++ client/src/ui/ForumCard.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import api from '../api';
 import { formatDistanceToNow } from 'date-fns'; // Import this for relative time formatting
+import './ForumCard.scss';
 
 export default function ForumCard() {
   const [data, setData]: [any, any] = useState(null);
@@ -12,32 +13,6 @@ export default function ForumCard() {
     });
   }, []);
 
-  const cardStyle = {
-    border: '1px solid #ddd',
-    borderRadius: '10px',
-    paddingLeft: '5px',
-    paddingRight: '5px',
-    margin: '10px',
-    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
-    transition: '0.3s',
-    backgroundColor: '#f9f9f9',
-    cursor: 'pointer',
-  };
-
-  const titleStyle = {
-    fontSize: '16px',
-    fontWeight: 'bold',
-    color: '#333',
-    marginBottom: '0px',
-    marginTop: '5px'
-  };
-
-  const infoStyle = {
-    fontSize: '12px',
-    color: '#666',
-    marginBottom: '5px',
-    marginTop: '0px'
-  };
 
   const redirectToTopic = (topicId: number) => {
     // window.location.href = `https://iogames.forum/t/${topicId}`;
@@ -54,10 +29,10 @@ export default function ForumCard() {
     <div>
         <h1 style={{margin: 0, marginTop: '5px'}}>Top Community Posts</h1>
       {data && data?.topic_list?.topics && data.topic_list.topics.slice(0, 3).map((topic: any) => (
-        <div className="forum-card" key={topic.id} style={cardStyle} onClick={() => redirectToTopic(topic.id)}>
-          <h3 style={titleStyle}>{topic.title}</h3>
-          <p style={infoStyle}>Posted by {userNameFromId(topic.posters.find((e: {description:string})=>e.description.includes("Original Poster"))?.user_id)} | {topic.views} Views | {formatDistanceToNow(new Date(topic.created_at))} ago</p>
-        </div>
+       <div className="forum-card" key={topic.id} onClick={() => redirectToTopic(topic.id)}>
+       <h3 className="forum-title">{topic.title}</h3>
+       <p className="forum-info">Posted by {userNameFromId(topic.posters.find((e: {description:string}) => e.description.includes("Original Poster"))?.user_id)} | {topic.views} Views | {formatDistanceToNow(new Date(topic.created_at))} ago</p>
+     </div>
       ))}
       {(!data || !data?.topic_list?.topics || data?.topic_list?.topics?.length === 0) && <div>None found :(</div>}
     </div>

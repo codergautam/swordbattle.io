5f5e8a6ea4c3888023b7bf6e4b5a58a67303275f
Gautam Anand <gautamgxtv@gmail.com>
2024-01-31 06:10:11 -0600

    smaller build

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -198,6 +198,10 @@ module.exports = function (webpackEnv) {
         ? 'source-map'
         : false
       : isEnvDevelopment && 'cheap-module-source-map',
+    // externals: phaser and chart.js (they are available in the global scope and do not need to be bundled)
+    externals: {
+      phaser: 'Phaser',
+    },
     // These are the "entry points" to our application.
     // This means they will be the "root" imports that are included in JS bundle.
     entry: paths.appIndexJs,
@@ -419,7 +423,7 @@ module.exports = function (webpackEnv) {
                     },
                   ],
                 ],
-                
+
                 plugins: [
                   isEnvDevelopment &&
                     shouldUseReactRefresh &&
@@ -453,7 +457,7 @@ module.exports = function (webpackEnv) {
                 cacheDirectory: true,
                 // See #6846 for context on why cacheCompression is disabled
                 cacheCompression: false,
-                
+
                 // Babel sourcemaps are needed for debugging into node_modules
                 // code.  Without the options below, debuggers like VSCode
                 // show incorrect code and set breakpoints on the wrong lines.

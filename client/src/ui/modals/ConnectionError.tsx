c44ab53532dea8bdcb457c2c5747bf26fa5e41d7
Gautam Anand <gautamgxtv@gmail.com>
2024-01-31 05:54:18 -0600

    z index

--- client/src/ui/modals/ConnectionError.tsx
+++ client/src/ui/modals/ConnectionError.tsx
@@ -3,7 +3,7 @@ import './ConnectionError.scss';
 export default function ConnectionError({ reason = 'Connection failed' }: any) {
   const reload = () => window.location.reload();
   return (
-    <div className="panel content">
+    <div className="panel content" style={{ zIndex: 100000 }}>
       <h2>Oops!</h2>
       <p>Server disconnected: {reason}</p>
       <p>Please try reconnecting.</p>

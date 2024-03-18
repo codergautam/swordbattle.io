2118317bf825e7f0ddc3dbf3abdee17ef32b9e60
Gautam Anand <gautamgxtv@gmail.com>
2024-03-17 22:17:54 -0500

    Done

--- client/src/ui/modals/ChangelogModal.tsx
+++ client/src/ui/modals/ChangelogModal.tsx
@@ -3,20 +3,9 @@ import './ChangelogModal.scss';
 function ChangelogModal() {
   return (
     <div className="changelog">
-      <h1 style={{fontSize: 30}}>DEGRADED PERFORMANCE</h1>
-      <ul>
-        <li>
-          <h3 style={{color: 'cyan', fontStyle: 'italic'}}>
-            We are currently investigating a memory leak that is causing servers to crash. <br/>You might get disconnect more often than usual.<br/>We are sorry for the inconvenience and are working on a fix.
-
-            <br/>
-            - Gautam @ swordbattle.io
-          </h3>
-        </li>
-        <hr />
-      </ul>
+      <h1 style={{fontSize: 30}}>Added a few new skins</h1>
       <a className="primary-link" target="_blank" href="https://iogames.forum/t/how-to-make-your-own-swordbattle-io-skin/585">
-    Create your own skins!</a>
+    Create your own skins to be added in the game!</a>
     </div>
   );
 }

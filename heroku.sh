098cb50e165e4856aa0c9297f010e6d33ff7e69a
Gautam Anand <gautamgxtv@gmail.com>
2024-08-05 15:46:25 -0500

    dgfd

--- heroku.sh
+++ heroku.sh
@@ -21,11 +21,11 @@ cd ..
 
 # Start API process
 cd api
-echo "Building API..."
-yarn build
-# run node dist/main in background
-echo "Starting processes with PM2..."
-pm2 start node --name api -- dist/main
+# echo "Building API..."
+# yarn build
+# # run node dist/main in background
+# echo "Starting processes with PM2..."
+# pm2 start node --name api -- dist/main
 
 # Start server process
 cd ../server

c9147c4756268e37649f8f6e6b686b653c8c7e7f
Gautam Anand <gautamgxtv@gmail.com>
2024-08-05 16:08:00 -0500

    dfgd

--- herokustart.sh
+++ herokustart.sh
@@ -1,11 +1,11 @@
 export API_PORT=3000
 export SERVER_PORT=8080
 cd api
-# echo "Building API..."
-# yarn build
-# # run node dist/main in background
-# echo "Starting processes with PM2..."
-# pm2 start node --name api -- dist/main
+echo "Building API..."
+yarn build
+# run node dist/main in background
+echo "Starting processes with PM2..."
+pm2 start node --name api -- dist/main
 
 # Start server process
 cd ../server

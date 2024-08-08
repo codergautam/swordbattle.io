e6364419abf1724e84e201409f50a9443ea9296f
Gautam Anand <gautamgxtv@gmail.com>
2024-08-08 13:00:29 -0500

    CHangelog modal

--- client/src/ui/modals/ChangelogModal.tsx
+++ client/src/ui/modals/ChangelogModal.tsx
@@ -3,14 +3,11 @@ import './ChangelogModal.scss';
 function ChangelogModal() {
   return (
     <div className="changelog">
-    <h1 style={{fontSize: 30}}>Evolution Revamp! (May 11th, 2024)</h1>
-    <li>- 4 new evolutions added!</li>
-    <li>- Balancing and improvements to PvP</li>
-    <li>- Improved Mobile Controls</li>
-    {/* <a className="primary-link" target="_blank" href="https://iogames.forum/t/how-to-make-your-own-swordbattle-io-skin/585">
+    <h1 style={{fontSize: 30}}>What's new? (Aug 8th, 2024)</h1>
+    <li>- New skins added!</li>
+    <a className="primary-link" target="_blank" href="https://iogames.forum/t/how-to-make-your-own-swordbattle-io-skin/585">
         Create your own skins to be added in the game!
-    </a>*/}
-    <p>Stay tuned for more updates and future projects from our team.</p>
+    </a>
 </div>
 
   );

865b1a68b761581658896436ffb14aa79c9568bc
Gautam Anand <gautamgxtv@gmail.com>
2024-09-23 07:40:32 -0500

    Fix

--- client/src/ui/modals/ChangelogModal.tsx
+++ client/src/ui/modals/ChangelogModal.tsx
@@ -3,8 +3,8 @@ import './ChangelogModal.scss';
 function ChangelogModal() {
   return (
     <div className="changelog">
-    <h1 style={{fontSize: 30}}>What's new? (Aug 8th, 2024)</h1>
-    <li>- New skins added!</li>
+    <h1 style={{fontSize: 30}}>What's new? (Sep 23rd, 2024)</h1>
+    <li>- New skins added! (Credit to Hue)</li>
     <a className="primary-link" target="_blank" href="https://iogames.forum/t/how-to-make-your-own-swordbattle-io-skin/585">
         Create your own skins to be added in the game!
     </a>

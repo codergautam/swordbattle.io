71ac084878ee07c2bc26a76e7f47a2a4c5472c61
Yocto <yoctoproductions@gmail.com>
2024-11-24 14:38:26 -0700

    More

--- client/src/ui/modals/ChangelogModal.tsx
+++ client/src/ui/modals/ChangelogModal.tsx
@@ -4,7 +4,7 @@ function ChangelogModal() {
   return (
     <div className="changelog">
     <h1 style={{fontSize: 30}}>What's new? (November 2024)</h1>
-    <li>- 55 NEW SKINS!! (Huge credit to cool guy 53)</li>
+    <li>- 60 NEW SKINS!! (Huge credit to cool guy 53)</li>
     <a className="primary-link" target="_blank" href="https://iogames.forum/t/how-to-make-your-own-swordbattle-io-skin/585">
         Create your own skins to be added in the game!
     </a>

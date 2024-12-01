5742116ce7200f87bb2d7e91491eaff9ce5d5b8a
Yocto <yoctoproductions@gmail.com>
2024-12-01 13:31:52 -0700

    Skins
    and changelog update

--- client/src/ui/modals/ChangelogModal.tsx
+++ client/src/ui/modals/ChangelogModal.tsx
@@ -4,7 +4,14 @@ function ChangelogModal() {
   return (
     <div className="changelog">
     <h1 style={{fontSize: 30}}>What's new? (December 2024)</h1>
-    <li>WINTER UPDATE! New pine tree forest & gingerbread biomes!</li>
+    <li>WINTER UPDATE! Enjoy these additions to the game during this event:</li>
+    <br></br>
+    <ul>- New pine tree forest & gingerbread biomes!</ul>
+    <ul>- Higher chances of rarer chests!</ul>
+    <ul>- Faster-spawning bosses!</ul>
+    <ul>- SKIN SALE! Save hundreds to thousands of gems on winter-themed skins! (Search OFF in the shop to quickly find these skins!)</ul>
+    <ul>- STRONGER MOBS! All mobs have increased health, damage or speed and drop much more coins!</ul>
+    <li>- EVENT SKINS! Buy limited-time winter skins before they go offsale! (Search WINTER EVENT in the shop to quickly find these skins!)</li>
 </div>
 
   );

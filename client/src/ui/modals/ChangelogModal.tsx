73fdef226d1885ad4bd663793c562e285a458f79
Yocto <yoctoproductions@gmail.com>
2024-12-27 01:08:47 -0700

    Better clarity

--- client/src/ui/modals/ChangelogModal.tsx
+++ client/src/ui/modals/ChangelogModal.tsx
@@ -14,10 +14,11 @@ function ChangelogModal() {
     <hr></hr>
     <h1 style={{fontSize: 30, color: 'black'}} className='rainbow'>New feature: Ultimacy</h1>
     <ul style={{fontSize: 27, color: 'red'}}>- Make an account to start earning ULTIMACY!</ul>
-    <ul style={{fontSize: 17, color: 'yellow'}}>- Get tons of coins in a single game to get more and more ultimacy! More coins = EXPONENTIALLY more ultimacy!</ul>
-    <ul style={{fontSize: 17, color: 'lime'}}>(You might also get some good scores on the coins leaderboard while doing this too!)</ul>
-    <ul style={{fontSize: 19, color: 'cyan'}}>- Climb the brand-new Ultimacy Leaderboard and become the ultimate swordbattler!</ul>
-    <ul style={{fontSize: 19, color: 'rgb(37, 142, 255)'}}>- Use your ultimacy to unlock the new Ultimate Skins in the shop!</ul>
+    <ul style={{fontSize: 17, color: 'orange'}}>- Get tons of coins in a single game to get more and more ultimacy!</ul>
+    <ul style={{fontSize: 17, color: 'yellow'}}>(You might also get some good scores on the coins leaderboard while doing this too!)</ul>
+    <ul style={{fontSize: 19, color: 'lime'}}>- Climb the brand-new Ultimacy Leaderboard and become the ultimate swordbattler!</ul>
+    <ul style={{fontSize: 19, color: 'cyan'}}>- Use your ultimacy to unlock the new Ultimate Skins in the shop!</ul>
+    <ul style={{fontSize: 16, color: 'rgb(37, 142, 255)'}}>(Tip: when you're trying to get ultimacy, its always better to get one amazing score than multiple smaller scores!)</ul>
     <br></br>
     <ul style={{fontSize: 12, color: 'magenta'}}>- (P.S. Changelog will also be reorganized soon, join the Discord to see the full changelog when it comes out!)</ul>
 </div>

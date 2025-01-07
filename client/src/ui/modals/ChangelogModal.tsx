b159e8d6a3d9930bdd613c97e2a4b3d6285fdb12
Yocto <yoctoproductions@gmail.com>
2025-01-06 18:22:32 -0700

    Changelog change

--- client/src/ui/modals/ChangelogModal.tsx
+++ client/src/ui/modals/ChangelogModal.tsx
@@ -12,6 +12,8 @@ function ChangelogModal() {
     <br></br>
     <ul style={{fontSize: 19, color: 'yellow'}}>- Climb the brand-new Mastery Leaderboard and become the master swordbattler!</ul>
     <ul style={{fontSize: 19, color: 'orange'}}>- Use your mastery to unlock the new Ultimate Skins in the shop!</ul>
+    <br></br>
+    <p style={{fontSize: 17, color: 'lightblue'}}>If you want more info on the winter update and mastery update (plus see other updates you may have missed), join the <a href="https://discord.com/invite/9A9dNTGWb9" className="primary-link" target="_blank" rel="nofollow">Swordbattle.io Discord Server</a>!</p>
 </div>
 
   );

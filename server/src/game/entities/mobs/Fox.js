f2a648b669806575fb2549c03b59a1d074dc744b
A-bot-Moonblade <moonblade696@gmail.com>
2025-01-09 12:08:29 -0800

    Update Fox.js

--- server/src/game/entities/mobs/Fox.js
+++ server/src/game/entities/mobs/Fox.js
@@ -19,7 +19,7 @@ class FoxMob extends Entity {
 
     this.shape = Circle.create(0, 0, this.size);
     this.angle = helpers.random(-Math.PI, Math.PI);
-    this.coinsDrop = this.size;
+    this.coinsDrop = 650;
 
     this.tamedBy = null;
 

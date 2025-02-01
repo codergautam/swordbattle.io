e0569191101de00581b6ee88d0d936449d5742e3
Yocto <yoctoproductions@gmail.com>
2025-01-31 20:47:35 -0700

    Update AnnouncementCard.tsx

--- client/src/ui/AnnouncementCard.tsx
+++ client/src/ui/AnnouncementCard.tsx
@@ -1,7 +1,6 @@
 import { AccountState } from "../redux/account/slice";
 
 export default function AccountCard({account, onLogin, onSignup,}: {account: AccountState, onLogin: () => void, onSignup: () => void}) {
-  if(account.isLoggedIn) {
     <span id="logged-out">
                     <div>
                     <span className="announcedate">
@@ -10,14 +9,4 @@ export default function AccountCard({account, onLogin, onSignup,}: {account: Acc
                        {} 3 new ultimate skins! + <span className="announceimportant">GET THE ULTIMATE LUMINOUS SKIN FOR FREE BEFORE 2/3!</span>
                     </div>
                   </span>
-  } else return (
-    <span id="logged-out">
-                    <div>
-                    <span className="announcedate">
-                      (January 27th): 
-                    </span>
-                       {} 3 new ultimate skins! + <span className="announceimportant">GET THE ULTIMATE LUMINOUS SKIN FOR FREE BEFORE 2/3!</span>
-                    </div>
-                  </span>
-  )
 }

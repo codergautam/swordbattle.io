068442983a21d5c86ac79c459cb71740db775f40
Yocto <yoctoproductions@gmail.com>
2025-02-13 23:43:23 -0700

    Update auth.service.ts

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -88,6 +88,9 @@ export class AuthService {
   async changeUsername(account: Account, newUsername: string) {
     try {
       let result = await this.accountsService.changeUsername(account.id, newUsername);
+      if(result.success) {
+        (result as any).secret = account.secret;
+      }
     return result;
     } catch (e) {
       console.log(e);
@@ -97,9 +100,6 @@ export class AuthService {
   async changeClan(account: Account, newClan: string) {
     try {
       let result = await this.accountsService.changeClan(account.id, newClan);
-      if(result.success) {
-        (result as any).secret = account.secret;
-      }
     return result;
     } catch (e) {
       console.log(e);

68ac29514213a196be6e168452c586342953d44b
Yocto <yoctoproductions@gmail.com>
2025-03-08 20:19:34 -0700

    balance 2

--- server/src/game/evolutions/Vampire.js
+++ server/src/game/evolutions/Vampire.js
@@ -6,17 +6,17 @@ module.exports = class Vampire extends Evolution {
   static level = 22;
   static previousEvol = Types.Evolution.Knight;
   // static level = 1;
-  static abilityDuration = 4;
-  static abilityCooldown = 75;
+  static abilityDuration = 4.5;
+  static abilityCooldown = 65;
 
   applyAbilityEffects() {
-    this.player.shape.setScale(0.875);
+    this.player.shape.setScale(0.925);
     this.player.modifiers.leech = 1.5;
-    this.player.sword.knockback.multiplier['ability'] = 1.8;
+    this.player.sword.knockback.multiplier['ability'] = 3;
     this.player.speed.multiplier *= 1.55;
-    this.player.sword.damage.multiplier *= 1.25;
+    this.player.sword.damage.multiplier *= 1.45;
     this.player.sword.swingDuration.multiplier['ability'] = 1.5;
-    this.player.health.max.multiplier *= 0.95;
+    this.player.health.max.multiplier *= 0.925;
   }
 
   update(dt) {

644c5f08278ffdac74b3102fa8f2122dc72926d0
Yocto <yoctoproductions@gmail.com>
2025-03-10 02:57:08 -0600

    Update App.tsx

--- client/src/ui/App.tsx
+++ client/src/ui/App.tsx
@@ -307,10 +307,10 @@ function App() {
 
                 <div className="announcementCard menuCard panel">
                     <div>
-                    <span className="announcewarning">
-                      NOTE: The game is currently being tested for bugfixes. Expect the possibility disconnects/server restarts.
+                    <span className="">
+                      Tip: Using the health upgrade heals you a bit, too! (More HP = more heal!)
                     </span>
-                      {/* {} New ultimate skin! + <span className="announceimportant">Huge skin sale until 2/26!</span> Tip: Using the health upgrade heals you a bit, too! (More HP = more heal!) */}
+                      {/* {} New ultimate skin! + <span className="announceimportant">Huge skin sale until 2/26!</span> */}
                   
                     </div>
                   </div>

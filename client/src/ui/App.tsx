827e17fcef7eaf70e930b81120526685fcb7ca32
Yocto <yoctoproductions@gmail.com>
2025-05-14 16:32:22 -0600

    Revert "Update App.tsx"
    This reverts commit 5ec516c30759c01e3928251a41d8d56d73066e1b.

--- client/src/ui/App.tsx
+++ client/src/ui/App.tsx
@@ -314,7 +314,10 @@ function App() {
 
                 <div className="announcementCard menuCard panel">
                     <div>
-                       Sorry if you guys are disconnecting a lot right now but I'm trying to fix everything soon
+                    <span className="announcedate">
+                      (May 10th): 
+                    </span>
+                       {} <span style={{color: '#00ff00'}}>9 new skins & 18 new ultimate skins!</span> + <span className="announceimportant"> Huge ultimate skin sale! </span> Check in often for new skins!
                     </div>
                   </div>
                 {/* <!-- Play --> */}

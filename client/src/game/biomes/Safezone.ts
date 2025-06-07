6e9fe7a8e0e10322f50fc4b2e92402ede2f30c84
Yocto <128773191+YoctoProductions@users.noreply.github.com>
2025-06-07 01:57:38 -0600

    Update Safezone.ts

--- client/src/game/biomes/Safezone.ts
+++ client/src/game/biomes/Safezone.ts
@@ -7,7 +7,7 @@ class Safezone extends Biome {
     const size = 64;
     const texture = scene.textures.createCanvas('safezone', size, size);
     const ctx = texture!.getContext();
-    ctx.fillStyle = '#36B536';
+    ctx.fillStyle = '#999999';
     ctx.fillRect(0, 0, size, size);
     texture!.refresh();
   }

0c9023f315f272c81c8e15424425d32e2e4ef747
Yocto <128773191+YoctoProductions@users.noreply.github.com>
2025-06-16 14:20:58 -0600

    Update validateClantag.ts

--- api/src/helpers/validateClantag.ts
+++ api/src/helpers/validateClantag.ts
@@ -2,9 +2,13 @@ import { config } from "src/config";
 import * as filter from 'leo-profanity';
 
 export default function validateClantag(clantag: string): string {
+  const forbiddenTags = ["DEV", "ADMIN", "OWNER", "CODER"];
   // just to be safe
-  if(clantag === "__proto__" || clantag === "constructor" || clantag === "prototype" || clantag === "DEV" || clantag === "ADMIN" || clantag === "OWNER" || clantag === "CODER") {
+  if(clantag === "__proto__" || clantag === "constructor" || clantag === "prototype") {
     return "Clan is not allowed";
+  }
+  if (forbiddenTags.includes(clantag.toUpperCase();)) {
+    return "No";
   }
   // normal validation
   if(clantag.length < config.clanLength[0] || clantag.length > config.clanLength[1]) {

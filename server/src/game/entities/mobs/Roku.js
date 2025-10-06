571ff8c79c06e22acda8ef0fb70f58958d35a3df
Yocto <yoctoproductions@gmail.com>
2025-10-05 23:18:15 -0600

    Revert "Update Roku.js"
    This reverts commit 0c146f48c443ce7104321aafa46b30818e2e5848.

--- server/src/game/entities/mobs/Roku.js
+++ server/src/game/entities/mobs/Roku.js
@@ -160,6 +160,7 @@ class RokuMob extends Entity {
   remove() {
     super.remove();
     this.game.map.spawnCoinsInShape(this.shape, this.coinsDrop);
+    this.createInstance();
   }
 
   cleanup() {

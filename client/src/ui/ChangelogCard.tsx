626a08386cb75575835aed36f83e7afe79ca9719
Yocto <yoctoproductions@gmail.com>
2026-04-10 23:06:36 -0600

    Update ChangelogCard.tsx

--- client/src/ui/ChangelogCard.tsx
+++ client/src/ui/ChangelogCard.tsx
@@ -6,10 +6,10 @@ export default function ChangelogCard({ onViewChangelog }: { onViewChangelog?: (
   return (
     <span>
       <h1>News and Updates</h1>
-      <h2 style={{color: 'rgb(9, 255, 0)'}}>March Update</h2>
-      <ul style={{color: 'rgb(255, 230, 0)'}}>- REVAMPED upgrades system!</ul>
-      <ul>- New healthbars!</ul>
-      <ul>- Other bugfixes and improvements</ul>
+      <h2 style={{color: 'rgb(0, 140, 255)'}}>CLAN UPDATE!</h2>
+      <ul style={{color: 'rgb(255, 230, 0)'}}>- New clans system!</ul>
+      <ul>- Better upgrades!</ul>
+      <ul>- More performance improvements</ul>
 
       {/* <a className="changelogbutton" onClick={onViewChangelog} style={{ cursor: 'pointer' }}>
         <FontAwesomeIcon icon={faClipboardList} /> View Changelog

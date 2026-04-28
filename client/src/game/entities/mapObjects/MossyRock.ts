aa83b50af398af4f51858c1a9acc95fdc24e4443
Yocto <yoctoproductions@gmail.com>
2026-04-27 18:05:20 -0600

    Fix

--- client/src/game/entities/mapObjects/MossyRock.ts
+++ client/src/game/entities/mapObjects/MossyRock.ts
@@ -3,11 +3,19 @@ import { BaseEntity } from '../BaseEntity';
 class MossyRock extends BaseEntity {
   static stateFields = [...BaseEntity.stateFields, 'size'];
 
+  body!: Phaser.GameObjects.Sprite;
+  shadow!: Phaser.GameObjects.Sprite;
+
   createSprite() {
-    this.container = this.game.add.sprite(this.shape.x, this.shape.y, 'mossyRock').setOrigin(0.03, 0.96);
-    this.container.scale = (this.size * 1.05) / this.container.width;
+    this.body = this.game.add.sprite(0, 0, 'mossyRock').setOrigin(0.03, 0.96);
+    this.body.setScale((this.size * 1.05) / this.body.width);
+    this.shadow = this.createOutlineShadow('mossyRock', 0.03, 0.96);
+    this.syncOutlineShadow(this.shadow, this.body);
+    this.container = this.game.add.container(this.shape.x, this.shape.y, [this.shadow, this.body]);
     return this.container;
   }
+
+  updateRotation() {}
 }
 
 export default MossyRock;

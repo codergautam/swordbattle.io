93b314568e63007a80ba8d0a87734d4547db4455
Yocto <yoctoproductions@gmail.com>
2026-05-25 18:20:19 -0600

    Fix

--- client/src/game/entities/Snowball.ts
+++ client/src/game/entities/Snowball.ts
@@ -4,19 +4,15 @@ class Snowball extends BaseEntity {
   static stateFields = [...BaseEntity.stateFields, 'angle'];
   static baseAngle = -Math.PI / 2;
 
-  static shadowOffsetX = 15;
-  static shadowOffsetY = 15;
-
   body!: Phaser.GameObjects.Sprite;
   shadow!: Phaser.GameObjects.Sprite;
 
   createSprite() {
     this.body = this.game.add.sprite(0, 0, 'snowball').setOrigin(0.5, 0);
-    this.shadow = this.game.add.sprite(Snowball.shadowOffsetX, Snowball.shadowOffsetY, 'snowballShadow').setOrigin(0.5, 0);
-    this.shadow.setAlpha(0.08);
     const scale = (this.shape.radius * 2) / this.body.width;
     this.body.setScale(scale);
-    this.shadow.setScale(scale);
+    this.shadow = this.createOutlineShadow('snowball', 0.5, 0);
+    this.syncOutlineShadow(this.shadow, this.body);
     this.container = this.game.add.container(this.shape.x, this.shape.y, [this.shadow, this.body]);
     return this.container;
   }

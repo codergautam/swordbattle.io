e19676dbea680de9c46d4a30f0cf13c4f69b8d79
Yocto <128773191+YoctoProductions@users.noreply.github.com>
2026-08-01 03:10:29 -0600

    fix

--- client/src/ui/ChangelogCard.tsx
+++ client/src/ui/ChangelogCard.tsx
@@ -4,6 +4,24 @@ import { faClipboardList } from "@fortawesome/free-solid-svg-icons";
 
 export default function ChangelogCard({ onViewChangelog }: { onViewChangelog?: () => void }) {
   return (
-   <span><h1>News and Updates</h1><h2 style="color: rgb(255, 255, 0);">Update Coming Soon</h2><ul style="color: rgb(255, 255, 200);">The v3.0 update will be releasing on <span style="color: rgb(255, 200, 100);">August 4th. </span>We understand the annoyance of so many delays, but this date is the definite release date as the update only needs testing to be finished. Expect the game to undergo maintenance beforehand.</ul> </span>
-  )
+    <span>
+      <h1>News and Updates</h1>
+
+      <h2 style={{ color: "rgb(255, 255, 0)" }}>
+        Update Coming Soon
+      </h2>
+
+      <ul style={{ color: "rgb(255, 255, 200)" }}>
+        <li>
+          The v3.0 update will be releasing on{" "}
+          <span style={{ color: "rgb(255, 200, 100)" }}>
+            August 4th.
+          </span>{" "}
+          We understand the annoyance of so many delays, but this date is the
+          definite release date as the update only needs testing to be
+          finished. Expect the game to undergo maintenance beforehand.
+        </li>
+      </ul>
+    </span>
+  );
 }

4c15dc2bdad8938770d4c9f1ea9ff065024fd3fc
Yocto <128773191+YoctoProductions@users.noreply.github.com>
2026-08-01 02:59:36 -0600

    New release date

--- client/src/ui/ChangelogCard.tsx
+++ client/src/ui/ChangelogCard.tsx
@@ -4,14 +4,6 @@ import { faClipboardList } from "@fortawesome/free-solid-svg-icons";
 
 export default function ChangelogCard({ onViewChangelog }: { onViewChangelog?: () => void }) {
   return (
-    <span>
-      <h1>News and Updates</h1>
-      <h2 style={{color: 'rgb(255, 0, 0)'}}>Update Coming Soon</h2>
-      <ul style={{color: '#ffffff'}}>Swordbattle's next huge update will be releasing soon, <span style={{color: '#ff7a7a'}}>no later than</span> June 27th. Game may go under maintenance for testing and debugging.</ul>
-
-      {/* <a className="changelogbutton" onClick={onViewChangelog} style={{ cursor: 'pointer' }}>
-        <FontAwesomeIcon icon={faClipboardList} /> View Changelog
-      </a> */} {/* actual changelog page won't be updated for a while */}
-    </span>
+   <span><h1>News and Updates</h1><h2 style="color: rgb(255, 255, 0);">Update Coming Soon</h2><ul style="color: rgb(255, 255, 200);">The v3.0 update will be releasing on <span style="color: rgb(255, 200, 100);">August 4th. </span>We understand the annoyance of so many delays, but this date is the definite release date as the update only needs testing to be finished. Expect the game to undergo maintenance beforehand.</ul> </span>
   )
 }
